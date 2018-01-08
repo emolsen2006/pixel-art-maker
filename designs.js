7ef2afd359a537849340064797bf4ac0800d60ff
Eric_Olsen <emolsen2006@yahoo.com>
2018-01-08 06:28:57 +0300

    docs: updated comments, descriptions, and checked against style guide

--- designs.js
+++ designs.js
@@ -1,10 +1,27 @@
+/*Author: Eric Olsen
+  Date: 8 Jan 2018
+  Description: This javascript file takes two form inputs and generates a
+  blank table according to user provided dimensions. It also allows the user
+  to click and drag color settings. This is an html table and the color is
+  using the CSS background (this does not use HTML canvas). This code also
+  does not utilize JQuery. I don't know enough about javascript yet to start
+  relying on a library. There is a slight bug remaining with the mouse cursor.
+  Sometimes the browser thinks that the mouse is clicking and dragging the
+  table element. This trips the mouse event listeners and causes the program to
+  miss the 'mouseup' listener. The result is that you continue drawing on
+  the table with the mouse button up. I think using HTML canvas would be the
+  true solution. I tried enhancing the CSS with the hover status, but this
+  started affecting the column width in the table and made the experience a
+  little weird, so I left it out.
+*/
 
-
-var submit = document.getElementById('submit-button'); //declare submit as var
+const submit = document.getElementById('submit-button'); //declare submit as var
 submit.addEventListener('click', makeGrid);
 
-var mouseDown = false;
-var canvas = document.querySelector('body');
+let mouseDown = false;  //helper variable for multiple event listeners
+const canvas = document.querySelector('body'); //track event over entire body
+
+//these two listeners will enable click and draw drawing
 canvas.addEventListener('mousedown', function() {
   mouseDown = true;
 });
@@ -12,37 +29,35 @@ canvas.addEventListener('mouseup', function() {
   mouseDown = false;
 });
 
-
+//grid generator method
 function makeGrid(e) {
-  e.preventDefault();   //block form submission to itself
-  var table = document.getElementById('pixel_canvas');  //set table reference var
-  //remove an existing grid
+  e.preventDefault();   //block form's default submission
+  let table = document.getElementById('pixel_canvas');  //set table reference var
+
+  //clear any existing grid
   if (table.hasChildNodes()){
     table = clearTable(table);
   }
-  //declare the 2 variable that we need
-  var gridWidth = document.getElementById('input_width').value;
-  var gridHeight = document.getElementById('input_height').value;
+
+  //declare the 2 variable for width and height
+  let gridWidth = document.getElementById('input_width').value;
+  let gridHeight = document.getElementById('input_height').value;
+
   //loop through gid creation
-  for (var i = 0; i < gridHeight; i++) {
-    var newRow = document.createElement('tr');
-    for (var j = 0; j < gridWidth; j++) {
-      var newCol = document.createElement('td')
-      newRow.appendChild(newCol);
-      addEvents(newCol);  //use function to add multiple event listeners
+  for (let i = 0; i < gridHeight; i++) {
+    let newRow = document.createElement('tr');
+    for (let j = 0; j < gridWidth; j++) {
+      let newCol = document.createElement('td')
+      newRow.appendChild(newCol); //add new cell to row
+      addEvents(newCol);  //add event listners inside the created cell
     }
-    table.appendChild(newRow);
+    table.appendChild(newRow); //add completed row to table
   }
 }//end of make grid
 
 function clearTable(table) {
   table.innerHTML = "";       //not pretty, but it works
   return table;
-}
-
-function getColor() {
-  var color = document.getElementById('colorPicker').value;
-  return color;
 }
 
 //adds pixel event listeners
@@ -52,4 +67,10 @@ function addEvents(tableCell) {
       tableCell.style.backgroundColor = getColor();
     }
   });
+}
+
+//called by cell event listener, basic getter for color setting
+function getColor() {
+  var color = document.getElementById('colorPicker').value;
+  return color;
 }
