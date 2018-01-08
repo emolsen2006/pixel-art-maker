fa836b20871c6887d704b1111bac8fc5f3a6140a
Eric_Olsen <emolsen2006@yahoo.com>
2018-01-08 05:52:05 +0300

    Feat: Mouse event listeners complete.
    Note: There is a slight bug that remains. Within the generated table
    sometimes the drag option is selected. The 100% solution is to not
    use a table and background color, but to use the HTML5 canvas tag.

--- designs.js
+++ designs.js
@@ -1,41 +1,34 @@
-// Select color input
-// Select size input
 
-/* Order of development:
-  1. Submit button event listener / call to makeGrid method
-  2. Grab table dimensions and add html element (no jQuery)
-  3. Set new event listeners (submit clears and grabs color)
-*/
 
 var submit = document.getElementById('submit-button'); //declare submit as var
 submit.addEventListener('click', makeGrid);
 
-function getColor() {
-  var color = document.getElementById('colorPicker').value;
-  return color;
-}
+var mouseDown = false;
+var canvas = document.querySelector('body');
+canvas.addEventListener('mousedown', function() {
+  mouseDown = true;
+});
+canvas.addEventListener('mouseup', function() {
+  mouseDown = false;
+});
+
 
 function makeGrid(e) {
   e.preventDefault();   //block form submission to itself
-
   var table = document.getElementById('pixel_canvas');  //set table reference var
-
   //remove an existing grid
-  console.log(table.hasChildNodes()); //white space must be removed in HTML
   if (table.hasChildNodes()){
     table = clearTable(table);
   }
-
   //declare the 2 variable that we need
   var gridWidth = document.getElementById('input_width').value;
   var gridHeight = document.getElementById('input_height').value;
-
   //loop through gid creation
-  for (var i = 0; i < gridHeight; i++) {                //row loop
-    var newRow = document.createElement('tr');          //create new row
-    for (var j = 0; j < gridWidth; j++) {               //col loop
-      var newCol = document.createElement('td')         //create new col
-      newRow.appendChild(newCol);                       //add col to row
+  for (var i = 0; i < gridHeight; i++) {
+    var newRow = document.createElement('tr');
+    for (var j = 0; j < gridWidth; j++) {
+      var newCol = document.createElement('td')
+      newRow.appendChild(newCol);
       addEvents(newCol);  //use function to add multiple event listeners
     }
     table.appendChild(newRow);
@@ -47,9 +40,16 @@ function clearTable(table) {
   return table;
 }
 
+function getColor() {
+  var color = document.getElementById('colorPicker').value;
+  return color;
+}
+
 //adds pixel event listeners
 function addEvents(tableCell) {
-  tableCell.addEventListener('mousemove', function(e) {
+  tableCell.addEventListener('mousemove', function() {
+    if (mouseDown){
       tableCell.style.backgroundColor = getColor();
+    }
   });
 }
